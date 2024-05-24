a091d1e51fd288dd87f5a32de91c829e973cdd74
roshan0o7 <123960047+roshan0o7@users.noreply.github.com>
2024-05-24 20:02:08 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# AWS region
+AWS_REGION="us-east-1"
+
+# AWS ECR repository URL
+ECR_REPO_URL="your-ecr-repo-url"
+
+# Docker image tag
+IMAGE_TAG="webserver"
+
+# AWS ECR login
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPO_URL
+
+# Pull the Docker image
+docker pull $ECR_REPO_URL:$IMAGE_TAG
+
+# Run the Docker container
+docker run -d -p 8080:8080 $ECR_REPO_URL:$IMAGE_TAG
